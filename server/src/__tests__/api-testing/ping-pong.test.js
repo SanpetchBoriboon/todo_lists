05a5731b84822297f42032d1462ec52388ad2999
Sanpetch Boriboon <ben@deeple.ai>
2022-08-17 14:26:39 +0700

    update package.json

--- server/src/__tests__/api-testing/ping-pong.test.js
+++ server/src/__tests__/api-testing/ping-pong.test.js
@@ -4,7 +4,13 @@ describe('Ping Pong', () => {
   it('send ping response pong', async () => {
     const result = await request.get('/ping')
     expect(result.status).toBe(200)
-    expect(result.body[0].message).toBe('pong')
+    expect(result.body).toEqual({
+      results: [
+        {
+          message: 'pong',
+        },
+      ],
+    })
   })
 
   it('send wrong url', async () => {
